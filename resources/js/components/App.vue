2fbecb263e9e422a315c954504378b40fbf73f3f
Cristina Ionescu <ionescu.ecristina@yahoo.ro>
2022-01-30 09:46:47 -0800

    initialization FilePond component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,14 +1,31 @@
 <template>
-<h1>Image Uploader</h1> 
-{{ message }}
+  <div>
+    <file-pond
+      name="image"
+      ref="pond"
+      label-idle="Click to choose image, or drag here..."
+      server="/upload"
+      @init="filepondInitialized"
+    />
+  </div>
 </template>
 
+
 <script>
+import vueFilePond from "vue-filepond";
+import "filepond/dist/filepond.min.css";
+
+//use vueFilePond function to create our component
+const FilePond = vueFilePond();
+
 export default {
-  data() {
-    return {
-        message: 'Hello World'
-    };
+  components: {
+    FilePond,
   },
+  methods: {
+    filepondInitialized() {
+       console.log('Filepond object', this.$refs.pond);
+    }
+  }
 };
 </script>
