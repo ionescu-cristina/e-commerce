7aa8ce17b0286eb24bc4ef5bfe4e1ffe8a9909c7
Ionescu Cristina <58083891+ionescu-cristina@users.noreply.github.com>
2022-02-16 10:59:29 -0800

    change architecture

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ImageController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::resource('/images', ImageController::class);
 
