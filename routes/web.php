b95d3d62a33869e3813c5d72326c29677729e418
Ionescu Cristina <58083891+ionescu-cristina@users.noreply.github.com>
2022-02-16 11:24:12 -0800

    add  product functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -13,3 +14,4 @@ Route::resource('/images', ImageController::class);
 
 Route::post('/upload', [ImageController::class, 'store']);
 
+Route::resource('products', ProductController::class);
